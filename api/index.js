4e8c3489f5294f2ea2f93e23b81c230b14a9e57d
Hacker <phoniegiwme@gmail.com>
2026-06-02 21:17:23 +0300

    chore: add global error handlers to prevent vercel crash

--- api/index.js
+++ api/index.js
@@ -1,1 +1,10 @@
-module.exports = require('../index');
+process.on('uncaughtException', (err) => { console.error('UNCAUGHT EXCEPTION:', err); });
+process.on('unhandledRejection', (reason) => { console.error('UNHANDLED REJECTION:', reason); });
+
+try {
+    module.exports = require('../index');
+} catch (error) {
+    module.exports = (req, res) => {
+        res.status(500).json({ error: "Startup Error", message: error.message, stack: error.stack });
+    };
+}
