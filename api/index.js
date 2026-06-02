db21a93832e7132275a2321e82c3d81ac0fa36b8
Hacker <phoniegiwme@gmail.com>
2026-06-03 00:12:44 +0300

    fix: lazy load index.js in vercel entrypoint to capture require errors

--- api/index.js
+++ api/index.js
@@ -1,12 +1,20 @@
 process.on('uncaughtException', (err) => { console.error('UNCAUGHT EXCEPTION:', err); });
 process.on('unhandledRejection', (reason) => { console.error('UNHANDLED REJECTION:', reason); });
 
-try {
-    module.exports = require('../index');
-} catch (error) {
-    module.exports = (req, res) => {
+module.exports = (req, res) => {
+    if (req.url && req.url.includes('test=1')) {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end("TEST_OK");
+        return;
+    }
+    
+    try {
+        const app = require('../index');
+        return app(req, res);
+    } catch (error) {
         res.statusCode = 500;
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify({ error: "Startup Error", message: error.message, stack: error.stack }));
-    };
-}
+    }
+};
